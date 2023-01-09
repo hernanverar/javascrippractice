c4f8505abbbef610bde69db734a6c590025083a7
Hernan Verar <hernanverar@gmail.com>
2023-01-09 10:44:26 -0800

    Js first practice file
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com

---
+++ jspractice.js
@@ -0,0 +1,18 @@
+function twoTimes(number1,number2) {
+    return number1/number2;
+}
+
+console.log(twoTimes(3,14));
+
+// function remainder(number1, number2) {
+//     let product = number1%number2;
+//     return "the anwers is " + product;
+// } 
+
+function remainder(number1, number2) {
+    
+    return "the anwers is " + number1 % number2;
+} 
+
+
+console.log(remainder(1,2));
